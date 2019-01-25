e1033494d1f6a4dd73c42293b745587a60a601bd
devnulled <devnulled@cryptaz.org>
2019-01-26 05:26:03 +0700

    EventComponent: add markup

--- src/app/components/EventComponent.js
+++ src/app/components/EventComponent.js
@@ -9,11 +9,72 @@ export default class EventComponent extends Component {
   render() {
 
     return (
-      <div style={divStyle}>
-      <ul>
-        <li>eventsList</li>
-      </ul>
-      
+      <div className="event-page">
+        <div className="event-page__content-container">
+          <div className="event-page__title">Традиционная игра на открытом воздухе</div>
+
+          <div className="event-page__main-block">
+            <div className="event-page-tabs event-page__tabs">
+              <div className="event-page-tabs__tab active">Информация</div>
+              <div className="event-page-tabs__tab">Задачи</div>
+              <div className="event-page-tabs__tab">Статистика</div>
+            </div>
+
+            <div className="event-info event-page__info disabled">
+              <div className="event-info__description">
+                CTF (Capture the flag) — это командная игра, главной целью которой является захват «флага» у
+                соперника. Формат Сapture the flag может использоваться и в пейнтболе, и среди ролевиков, и в
+                компьютерных играх. В области компьютерной безопасности тоже есть своё понятие CTF. Командные
+                соревнования по информационной безопасности формата CTF — это как раз то, что стало причиной
+                появления тысяч команд со всего мира и то, ради чего создана эта лента.
+              </div>
+
+              <div className="event-info__organizers">
+                Организатор: Газпром
+              </div>
+
+              <div className="event-info__sponsors-list sponsors-list">
+                <div className="sponsors-list__title">Спонсоры:</div>
+
+                <div className="sponsors-list__list-container">
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                </div>
+              </div>
+
+
+            </div>
+
+            <div className="event-page__tasks-list-block disabled">
+
+              <div className="task-categories-list event-page__categories-list">
+                <div className="task-categories-list__item">ppc</div>
+                <div className="task-categories-list__item">programming</div>
+                <div className="task-categories-list__item">hack</div>
+              </div>
+
+              <div className="tasks-list event-page__tasks-list">
+                <div className="tasks-list-item">
+                  <div className="tasks-list-item__count">200</div>
+                  <div className="tasks-list-item__name">Есть ещё один способ стать участником RuCTF</div>
+                </div>
+                <div className="tasks-list-item">
+                  <div className="tasks-list-item__count">200</div>
+                  <div className="tasks-list-item__name">Есть ещё один способ стать участником RuCTF</div>
+                </div>
+                <div className="tasks-list-item">
+                  <div className="tasks-list-item__count">200</div>
+                  <div className="tasks-list-item__name">Есть ещё один способ стать участником RuCTF</div>
+                </div>
+              </div>
+
+            </div>
 
             <div className="scoreboard event-page__scoreboard">
               <div className="scoreboard-item">
