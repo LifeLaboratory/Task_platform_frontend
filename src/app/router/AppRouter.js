9b87c7b0a0ae0567e4b52a387d0a8a6e023979cf
devnulled <devnulled@cryptaz.org>
2019-01-26 05:25:20 +0700

    AppRouter: fix imports

--- src/app/router/AppRouter.js
+++ src/app/router/AppRouter.js
@@ -2,8 +2,9 @@ import React from "react"
 import {Redirect, Route, Switch} from "react-router-dom"
 
 import {ConnectedRouter} from "connected-react-router";
+import EventListContainer from "../containers/EventListContainer";
+import HeaderContainer from "../containers/HeaderContainer";
 import EventContainer from "../containers/EventContainer";
-import EventsContainer from "../containers/EventsContainer";
 
 const NoMatch = () => <h2>404 (Not found)</h2>
 
@@ -11,10 +12,8 @@ const AppRouter = ({history}) => (
   <ConnectedRouter history={history}>
     <div className={"app_container"}>
       <Switch>
-        <Route path="/events" exact component={EventContainer}/>
-
-        <Redirect exact from="/" to="events" />
-        <Route path="/events" exact component={EventsContainer}/>
+        <Route path="/events" exact component={EventListContainer}/>
+        <Route path="/event/:id" exact component={EventContainer}/>
 
         <Redirect exact from="/" to="events"/>
 
