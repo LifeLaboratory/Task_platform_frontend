48139a322753cb4bdad3244bba3aa478863f66b8
devnulled <devnulled@cryptaz.org>
2019-01-26 05:46:27 +0700

    EventListComponent: pass mock data

--- src/app/components/EventListComponent.js
+++ src/app/components/EventListComponent.js
@@ -1,61 +1,13 @@
 import React, {Component} from "react"
 
-
-const divStyle = {
-    margin: '100px',
-    height: '300px'
-};
-
 export default class EventListComponent extends Component {
   constructor(props) {
     super(props)
-
-    const {actions, auth} = props
-    this.actions = actions
-    this.auth = auth
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangeLogin = this.handleChangeLogin.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleChangeEmail = this.handleChangeEmail.bind(this);
-
-    this.state={
-        name:'',
-        login:'',
-        password:'',
-        email:''
-    }
-
-
   }
 
+  render(props) {
+    const {events, actions, auth} = this.props
 
-  handleSubmit(event){
-    event.preventDefault()
-    this.actions.loginAction({
-      login: 'test',
-      password: '123',
-    });
-
-  }
-
-  handleChangeName(event){
-        this.name = event.target.value
-    }
-
-  handleChangeLogin(event){
-        this.login = event.target.value
-    }
-
-  handleChangePassword(event){
-        this.password = event.target.value
-    }
-
-  handleChangeEmail(event){
-        this.email = event.target.value
-    }
-
-  render() {
     return (
 
       <div className="events-list-page">
@@ -80,60 +32,61 @@ export default class EventListComponent extends Component {
         </div>
 
         <div className="events-list">
-          <div className="events-list-item active">
-            <div className="events-list-item__head">
 
-              <div className="events-list-item__icon"></div>
+          {
+            events.map((event) =>
+              <div className="events-list-item active">
+                <div className="events-list-item__head">
 
-              <div className="events-list-item__date">
-                24.01.2019 -
-                24.01.2019
-              </div>
+                  <div className="events-list-item__icon"></div>
 
-              <div className="events-list-item__name">
-                Традиционная игра на открытом воздухе
-              </div>
+                  <div className="events-list-item__date">
+                    {event.date}
+                  </div>
 
-              <div className="events-list-item__count">
-                3 / 10
-              </div>
+                  <div className="events-list-item__name">
+                    {event.name}
+                  </div>
 
-              <div className="events-list-item__btn reg">
-                регистрация
-              </div>
-            </div>
+                  <div className="events-list-item__count">
+                    {event.players}
+                  </div>
 
-            <div className="events-list-item__dropdown event-info">
-              <div className="event-info__description">
-                CTF (Capture the flag) — это командная игра, главной целью которой является захват «флага» у
-                соперника. Формат Сapture the flag может использоваться и в пейнтболе, и среди ролевиков, и в
-                компьютерных играх. В области компьютерной безопасности тоже есть своё понятие CTF. Командные
-                соревнования по информационной безопасности формата CTF — это как раз то, что стало причиной
-                появления тысяч команд со всего мира и то, ради чего создана эта лента.
-              </div>
+                  <div className="events-list-item__btn reg">
+                    регистрация
+                  </div>
+                </div>
 
-              <div className="event-info__organizers">
-                Организатор: Газпром
-              </div>
+                <div className="events-list-item__dropdown event-info">
+                  <div className="event-info__description">
+                    {event.description}
+                  </div>
 
-              <div className="event-info__sponsors-list sponsors-list">
-                <div className="sponsors-list__title">Спонсоры:</div>
+                  <div className="event-info__organizers">
+                    Организатор: {event.organizer.name}
+                  </div>
 
-                <div className="sponsors-list__list-container">
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
-                  <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                  <div className="event-info__sponsors-list sponsors-list">
+                    <div className="sponsors-list__title">Спонсоры:</div>
+
+                    <div className="sponsors-list__list-container">
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                      <a className="sponsors-list__item"><img src="img/megafon.png" alt=""/></a>
+                    </div>
+                  </div>
+
+
                 </div>
               </div>
+            )
+          }
 
-
-            </div>
-          </div>
         </div>
       </div>
 
--- src/app/containers/EventListContainer.js
+++ src/app/containers/EventListContainer.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import EventListComponent from '../components/EventListComponent'
 import loginActionCreator from '../actions/creators/loginActionCreator'
-import { bindActionCreators } from "redux"
+import {bindActionCreators} from "redux"
 
 class EventListContainer extends Component {
   constructor(props) {
@@ -17,13 +17,34 @@ class EventListContainer extends Component {
     }
 
     this.auth = auth
+    this.events = [{
+      date: "24.01.2019 - 24.01.2019",
+      name: "Традиционная игра на открытом воздухе",
+      players: "3/10",
+      description: `CTF (Capture the flag) — это командная игра, главной целью которой является захват «флага» у соперника. 
+      Формат Сapture the flag может использоваться и в пейнтболе, и среди ролевиков, и в компьютерных играх. 
+      В области компьютерной безопасности тоже есть своё понятие CTF. 
+      Командные соревнования по информационной безопасности формата CTF — это как раз то,
+       что стало причиной появления тысяч команд со всего мира и то, ради чего создана эта лента.`,
+      organizer: {
+        name: "Gazprom"
+      },
+      sponsors: [
+        {
+          name: "Megafon"
+        }
+      ]
+    }]
   }
 
+
   render() {
-    return (<EventListComponent actions={this.actions} auth={this.props.auth}/>)
+
+    return (<EventListComponent actions={this.actions} auth={this.props.auth} events={this.events}/>)
   }
 }
+
 const mapStateToProps = (state) => {
-  return {auth:state.auth}
+  return {auth: state.auth}
 }
 export default connect(mapStateToProps)(EventListContainer)
